c46f9c3f3265fc813cc9d1bdaa1c6765c39760b6
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2018-12-06 09:05:51 -0300

    Dup count

---
+++ dupCount.cpp
@@ -0,0 +1,39 @@
+#include "tiffio.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Usage: ./count name
+int main(int argc, char* argv[]){
+    
+    if(argc != 2) {
+        cerr << "Invalid number of arguments!" << endl;
+        return 0;
+    }
+    
+    TIFF* tif;
+    tif = TIFFOpen(argv[1], "rm");
+
+    int width, height;
+
+    TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);
+    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);
+
+    double* line = new double[width];
+    set<double> distinct;
+    int valid = 0;
+    for(int row = 0; row < height; row++){
+        TIFFReadScanline(tif, line, row);
+        for(int column = 0; column < width; column++){
+            if(!isnan(line[column])){
+                distinct.insert(line[column]);
+                valid += 1;
+            }
+        }
+    }
+
+    printf("%.1lf\n", 1 - 1.0 * distinct.size() / valid);
+    TIFFClose(tif);
+    return 0;
+}
